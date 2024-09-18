a9a4a3a35b8e1317c75a38f0147d5d00e272c1f7
ApxNord <dronov_a@t.top-academy.ru>
2024-09-18 21:13:30 +0300

    Добавьте файлы проекта.

---
+++ Practic18.09.2024/Extend.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic18._09._2024
+{
+    abstract class Human
+    {
+        private int _id;
+        public string Name { get; set; }
+        public DateTime BirthDay { get; set; }
+        protected string _surname = "Иванов";      
+
+        public Human(string name, string date) {
+            Name = name;
+            BirthDay = DateTime.Parse(date);
+        }
+        public abstract void Print();
+    }
+
+    class Employee : Human
+    {
+        public Employee(string name, string date,
+            int salary, string pos) : base(name, date)
+        {
+            Salary = salary;
+            Position = pos;
+        }
+        public Employee(string name, string date): this(name, date, 0, null){}
+        public int Salary { get; set; }
+        public string Position { get; set; }
+
+        public virtual void Show()
+        {
+            Console.WriteLine($"{Salary}, {Position}, {Name} {_surname}");
+        }
+
+        public override void Print() {         
+           Console.WriteLine($"{Salary}, {Position}");
+        }
+    }
+    sealed class Storekeeper : Employee
+    {
+        public Storekeeper(string name, string date, int salary, string pos)
+            : base(name, date, salary, pos)
+        {
+        }
+        public void ShowStore()
+        {
+            Console.WriteLine("Складирую товар");
+        }
+        public string Description { get; set; }
+
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine($" описание должности");
+        }
+        /// <returns>string</returns>
+        private string PrintNewMethod()
+        {
+            return $"{Name + " " + BirthDay.ToLongDateString()}\n" +
+                $"{Salary}, {Position}\n" +
+                $"описание должности";
+        }
+        public override string ToString()
+        {
+            return $"{Name + " " + BirthDay.ToLongDateString()}\n" +
+                $"{Salary}, {Position}\n" +
+                $"описание должности";
+        }
+
+    }
+    class Cashier : Employee
+    {
+        public Cashier(string name, string date, int salary, string pos)
+            : base(name, date, salary, pos)
+        {
+        }
+        public void ShowKassa()
+        {
+            Console.WriteLine("Пробиваю товар");
+        }
+        public string Description { get; set; }
+
+    }
+    class Saler : Employee
+    {
+        public Saler(string name, string date, int salary, string pos)
+            : base(name, date, salary, pos)
+        {
+        }
+        public void ShowSale()
+        {
+            Console.WriteLine("Продаю товар");
+        }
+        public string Description { get; set; }
+
+    }
+
+    /*    class Test : Storekeeper // нельзя унаследоваться, т.к. Storekeeper имеет sealed
+        {
+            public Test(string name, string date, int salary, string pos) : base(name, date, salary, pos)
+            {
+            }
+        }*/
+
+    /*abstract class AbstractClass
+    {
+        public string Name { get; set; }
+        private int number;
+
+        public void Show()
+        {
+            Console.WriteLine("Метод абстрактного класса");
+        }
+        //public abstract void Print();
+    }*/
+}
---
+++ Practic18.09.2024/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic18._09._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee storekeeper = new Storekeeper(
+                "Александр",
+                "07.03.1960",
+                56000,
+                "storekeeper");
+            /*Employee[] employee =
+            {
+                new Cashier ("Кассир", "01-01-01", 45000, "cashier"),
+                new Saler ("Продавец", "06 12 1900", 45000, "saler"),
+                storekeeper
+            };
+
+            foreach (Employee emp in employee)
+            {
+                emp.Show();
+                // 1 способ
+                try
+                {
+                    ((Storekeeper)emp).ShowStore();
+                }
+                catch { }
+
+                // 2 способ
+                Cashier cashier = emp as Cashier;
+                if (cashier != null)
+                {
+                    cashier.ShowKassa();
+                }
+
+                // 3 способ
+                if (emp is Saler)
+                {
+                    (emp as Saler).ShowSale();
+                }
+            }*/
+
+            //storekeeper.Print();
+
+            //Console.WriteLine((Storekeeper)storekeeper).Print2());
+            Console.WriteLine(storekeeper);
+
+            Employee human = new Storekeeper("Александр",
+                "07.03.1960",
+                56000,
+                "storekeeper");
+
+            /*
+            Создать класс Животное (+ если класс будет абстрактным),
+            Создать дочерние классы: Кот, Собака
+            В main создать экземпляр класса Животное и передать 
+            ссылки на дочерние классы
+             */
+
+        }
+    }
+}
